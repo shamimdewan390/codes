05502830c74e7db17fdb09ec45af6935904a74fc
Shamim Dewan <49916445+hacksaw390@users.noreply.github.com>
2022-09-07 15:03:17 +0600

    Update image-preview.blade.php

--- image-preview.blade.php
+++ image-preview.blade.php
@@ -1,3 +1,7 @@
+       <label>
+            <span>Thumbnail (Optional. Thumbnail will croped by 1280x720px in center)</span>
+            <input type="file"  onchange="loadfile(event)"  name="image" class="form-control" id="image">
+        </label>
       <img src="/frontend/img/home-img/banner-01.jpg" id="preimage" width="270" width="325" style="background: #c3c3c3;height: 200px;width: 325px">
                                                        
     <script>

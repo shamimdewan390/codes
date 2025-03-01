90fd3715ec2ed559ca801c4e0a188cc06d64da39
Shamim Dewan <49916445+shamimdewan390@users.noreply.github.com>
2025-03-01 10:41:23 +0600

    Update paypal.php

--- paypal.php
+++ paypal.php
@@ -1,1 +1,18 @@
+personal account:
+shamimdewan343@gmail.com
+dewan390
+bussiness accoutn:
+moskhara390@gmail.com -> dewan390
 
+
+1. create developer account
+https://developer.paypal.com/home/ 
+moskhara390@gmail.com -> dewan390
+
+2. create sendbox accouon (bottom in home page)
+click sendbox account here we find bussiness accunt and Personal
+credentials found in 3 dot view section
+
+3. login sendbox account to anoter browser or incognito 
+https://www.sandbox.paypal.com/signin
+moskhara390@gmail.com -> dewan390
